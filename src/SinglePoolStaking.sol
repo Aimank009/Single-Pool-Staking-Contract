d555e688254f5eb082a79f1ba5a82d828fcfae3a
Aiman Khan <aiman@Aimans-MacBook-Air.local>
2026-01-12 03:26:47 +0530

    Add Claim and Exit

--- src/SinglePoolStaking.sol
+++ src/SinglePoolStaking.sol
@@ -20,12 +20,12 @@ contract SinglePoolStaking is IStaking,StakingEvents,Ownable,ReentrancyGuard{
 
     uint256 public constant PRECISION = 1e18;
 
-    struct USerInfo{
+    struct UserInfo{
         uint256 amount;
         uint256 rewardDebt;
     }
 
-    mapping(address => USerInfo) public users;
+    mapping(address => UserInfo) public users;
 
     constructor(
         address _stakingToken,
@@ -62,6 +62,84 @@ contract SinglePoolStaking is IStaking,StakingEvents,Ownable,ReentrancyGuard{
 
         accRewardPerShare+= (reward*PRECISION)/totalStaked;
         lastUpdateTime = block.timestamp;
+    }
+
+    function stake(uint256 _amount) external override nonReentrant{
+
+        require(_amount >0 ,"Stake amount should be greater than zero");
+
+        updatePool();
+        UserInfo storage user = users[msg.sender];
+
+        if(user.amount>0){
+            uint256 pending = (user.amount * accRewardPerShare)/PRECISION - user.rewardDebt;
+
+            if(pending>0){
+                require(rewardToken.transfer(msg.sender,pending),"Pending Transfer failder");
+
+                emit Claimed(msg.sender, pending);
+            }
+        }
+
+        require(stakingToken.transferFrom(msg.sender,address(this),_amount)," Stake Transfer Failed");
+
+        user.amount+=_amount;
+        totalStaked+=_amount;
+
+        user.rewardDebt=(user.amount*accRewardPerShare)/PRECISION;
+
+        emit Staked(msg.sender, _amount);
+
+    }
+
+    function withdraw(uint256 _amount) public override nonReentrant{
+        require(_amount>0,"Withdraw amount cannot be less than 0");
+
+        updatePool();
+
+        UserInfo storage user = users[msg.sender];
+
+        require(user.amount>=_amount,"Not enough staked");
+
+        uint256 pending = (user.amount*accRewardPerShare)/PRECISION - user.rewardDebt;
+
+        if(pending>0){
+            require(rewardToken.transfer(msg.sender,pending),"Pending Transfer Failed");
+
+            emit Claimed(msg.sender, pending);
+        }
+
+        user.amount-=_amount;
+        totalStaked-=_amount;
+
+        user.rewardDebt = (user.amount*accRewardPerShare)/PRECISION;
+        require(stakingToken.transfer(msg.sender,_amount),"Withdraw Transfer Failed");
+
+        emit Withdrawn(msg.sender, _amount);
+
+    }
+
+    function claim() external override{
+        UserInfo storage user = users[msg.sender];
+
+        uint256 pending = (user.amount*accRewardPerShare)/PRECISION -user.rewardDebt;
+
+        require(pending>0,"No Rewwards to claim");
+
+        user.rewardDebt=(user.amount*accRewardPerShare)/PRECISION;
+
+        require(rewardToken.transfer(msg.sender,pending),"Claim Failed");
+
+        emit Claimed(msg.sender, pending);
+    }
+
+    function exit() external override nonReentrant{
+
+        UserInfo storage user = users[msg.sender];
+
+        require(user.amount>0,"Nothing to exit");
+
+        withdraw(user.amount);
     }
 
 }
